03dab92b42dce197112deed8ac8eb2b24542b992
Seongwon Kim <seongwon.kim@modelical.com>
2025-01-27 20:01:00 +0900

    - add mock server

--- tests/api/metadata.test.ts
+++ tests/api/metadata.test.ts
@@ -1,9 +1,17 @@
+import { NextResponse } from 'next/server';
 import { GET } from '@/app/api/metadata/route';
 
 test('API should return metadata for images', async () => {
-  const res = await GET(); // API Route 호출
-  const data = await res.json();
+  // Mock `NextResponse`
+  const mockJson = jest.fn();
+  jest.spyOn(NextResponse, 'json').mockImplementation(mockJson);
 
+  // Call the API route
+  await GET();
+
+  // Verify the response
+  expect(mockJson).toHaveBeenCalled();
+  const [data] = mockJson.mock.calls[0];
   expect(data).toBeInstanceOf(Array);
   expect(data[0]).toHaveProperty('fileName');
   expect(data[0]).toHaveProperty('category');
