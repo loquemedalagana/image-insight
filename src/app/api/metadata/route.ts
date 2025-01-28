fa1cd09450acc069097f280144e65fa86088e9ae
Seongwon Kim <seongwon.kim@modelical.com>
2025-01-28 22:25:48 +0900

    - delete unused api

--- src/app/api/metadata/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import { extractMetadata } from '@/lib/extractMetadata';
-
-export async function GET() {
-  const metadata = await extractMetadata();
-  return NextResponse.json(metadata);
-}
