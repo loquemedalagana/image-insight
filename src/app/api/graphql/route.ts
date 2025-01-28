d74bb65da08c11a301da80fe803693b96d963679
Seongwon Kim <seongwon.kim@modelical.com>
2025-01-28 13:13:48 +0900

    - add playground option

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,21 +1,25 @@
 import { ApolloServer } from 'apollo-server-micro';
-import { typeDefs } from '@/graphql/schema/index';
-import { resolvers } from '@/graphql/resolvers/index';
+import { typeDefs } from '@/graphql/schema';
+import { resolvers } from '@/graphql/resolvers';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 
-const apolloServer = new ApolloServer({
+const server = new ApolloServer({
   typeDefs,
   resolvers,
+  csrfPrevention: true,
+  cache: 'bounded',
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground(), // Playground 활성화
+  ],
 });
-
-const startServer = apolloServer.start();
-
-export default async function handler(req: any, res: any) {
-  await startServer;
-  await apolloServer.createHandler({ path: '/api/graphql' })(req, res);
-}
 
 export const config = {
   api: {
     bodyParser: false,
   },
 };
+
+export default async function handler(req: any, res: any) {
+  await server.start();
+  await server.createHandler({ path: '/api/graphql' })(req, res);
+}
