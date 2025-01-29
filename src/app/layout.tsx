6cbbe623123ce0d589b6fb07ed5950ea9408b94d
Seongwon Kim <seongwon.kim@modelical.com>
2025-01-29 14:11:40 +0900

    - customized footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,50 +33,19 @@ export default function RootLayout({
           {children}
         </div>
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+          <div className="text-center">
+            <p>
+              © {new Date().getFullYear()} Image Insight by SWK. All rights
+              reserved.
+            </p>
+          </div>
           <a
             className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://github.com/loquemedalagana/image-insight"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
+            GitHub Repository
           </a>
         </footer>
       </body>
